ce184b7be1e8bb9741c4fade01026c53b6df8870
wells <jhang0912407249@gmail.com>
2022-04-11 15:10:14 +0800

    Add site config

---
+++ app/models/config.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'base.php';
+class Config
+{
+  public static $site_title;
+
+  public static function update_title($title){
+    $base = new Base();
+    $sql = $base->conn->prepare(<<<SQL
+      UPDATE `configs` SET `value`=? WHERE `key`='title';
+    SQL);
+    $sql->bind_param('s', $title);
+    return $sql->execute();
+  }
+
+  public static function setup(){
+    $base = new Base();
+    $sql = $base->conn->prepare(<<<SQL
+      SELECT * FROM `configs` WHERE `key` = 'title';
+    SQL);
+    $sql->execute();
+    $result = $sql->get_result();
+    if(mysqli_num_rows($result) == 0){
+      $base->execute_sql(<<<SQL
+        INSERT INTO `configs` (`key`, `value`) VALUES ('title', 'Web Security');
+      SQL);
+    }
+    self::$site_title = $result->fetch_assoc()['value'] ?? "Web Security";
+  }
+}
+
+?>
--- app/server.php
+++ app/server.php
@@ -1,5 +1,7 @@
 <?php
   require_once 'router.php';
+  require_once 'models/config.php';
+  Config::setup();
   $router = new Router('/posts');
 
   $router->run();
