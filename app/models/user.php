53d2b4ff69742c2f3b518ff0a7fa4c24173d7c25
wells <jhang0912407249@gmail.com>
2022-04-20 23:51:34 +0800

    Add initial image when user create

--- app/models/user.php
+++ app/models/user.php
@@ -9,7 +9,7 @@ class User
     $base = new Base();
     $encrypted_password = password_hash($attrs['password'], PASSWORD_DEFAULT);
     $sql = $base->conn->prepare(<<<SQL
-      INSERT INTO `users` (`email`, `username`, `password`, `role`) VALUES (?, ?, ?, 'user');
+      INSERT INTO `users` (`email`, `username`, `password`, `image_url`, `role`) VALUES (?, ?, ?, '/public/user_template.jpg', 'user');
     SQL);
     $sql->bind_param('sss', $attrs['email'], $attrs['username'], $encrypted_password);
     return $sql->execute();
